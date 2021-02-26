2bf3d083a1d5e8445158861f8da5a0bb128565fc
Yurii Kinakh <extrem7@meta.ua>
2021-02-26 16:56:15 +0200

    Remove logo alt from mail template

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="https://api.tattoostars.pro/admin/dist/img/logo.png" class="logo" alt="TattooStars Logo">
+<img src="https://api.tattoostars.pro/admin/dist/img/logo.png" class="logo">
 </a>
 </td>
 </tr>
