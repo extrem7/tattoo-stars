91c015c06f9108b9b61bd52528fcf45af1079b1e
Yurii Kinakh <extrem7@meta.ua>
2021-06-23 21:57:50 +0300

    feat(broadcasting): chat privat channel

--- routes/channels.php
+++ routes/channels.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Models\Chat\Chat;
 use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('messages.{id}', function (User $user, int $id) {
     return $user->id === $id;
+});
+
+Broadcast::channel('chat.{chat}', function (User $user, Chat $chat) {
+    return $chat->participants()->where('id', '=', $user->id)->exists();
 });
